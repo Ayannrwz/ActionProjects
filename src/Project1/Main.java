41aa8ff09cd70273513c8893e8749e069a2a4750
Ayannrwz <145316034+Ayannrwz@users.noreply.github.com>
2023-09-20 14:23:15 +0800

    Update Main.java
    Added scanner

--- src/Project1/Main.java
+++ src/Project1/Main.java
@@ -1,9 +1,11 @@
 package Project1;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
 
 	}
 
